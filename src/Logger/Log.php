86e7d1fc7f0d2eb32cae61215ef703cd1dc53f8a
Artem <arzek2015@gmail.com>
2019-01-21 23:14:38 +0200

    remove curl class

--- src/Logger/Log.php
+++ src/Logger/Log.php
@@ -8,8 +8,6 @@
 
 namespace Logger;
 
-use \Curl\Curl;
-
 /**
  * Class Log
  * @package Logger
@@ -23,13 +21,26 @@ class Log
      * @param $context
      */
     private static function sendLog($level, $message, $context) {
-        Curl::post(getenv('LOGGER_URL'), [
+        $data = [
             'token' => getenv('LOGGER_TOKEN'),
             'env' => self::getRealIpAddr(),
             'level' => $level,
             'message' => $message,
             'context' => $context
-        ]);
+        ];
+
+        $data_string = json_encode($data);
+
+        $ch = curl_init(getenv('LOGGER_URL'));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string))
+        );
+
+        $result = curl_exec($ch);
     }
 
     /**
